eeba16230d496cc574f266794ff18af0a2df31c8
Eric Griffis <dedbox@gmail.com>
2017-09-30 09:40:48 -0700

    remove custom background color

--- init.el
+++ init.el
@@ -203,4 +203,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background "#1f292e")))))
+ )
